3966d79366f2a799b4e60707a12f10b26387d8cc
Gavin Douglas <gavinmdouglas@gmail.com>
2023-05-30 09:56:28 -0400

    working

--- scripts/comp_mapping_postprocess/parse_breadth.R
+++ scripts/comp_mapping_postprocess/parse_breadth.R
@@ -1,71 +1,79 @@
-### Parse breadth of coverage from bedGraph files and add in useful summary info.
+# Parse breadth of coverage from bedGraph files and add in useful summary info.
+# Define genes as present based on calls of >= 50% coverage.
 
 rm(list = ls(all.names = TRUE))
 
+library(parallel)
 library(plyr)
 
-read_in_breadth_files <- function(in_path, pattern, roary_formatted_pangenome) {
-  
+read_in_breadth_files <- function(in_path, pattern, roary_formatted_pangenome, num_cores = 1) {
+
   in_path <- gsub("/$", "", in_path)
   
   # Read in all breadth files.
   input_breadth_files <- list.files(path = in_path, full.names = TRUE, pattern = pattern)
   
-  input_breadth_by_sample <- lapply(input_breadth_files, function(x) { read.table(x, header = FALSE, sep = "\t", row.names = 1, stringsAsFactors = FALSE) })
+  input_breadth_by_sample <- parallel::mclapply(input_breadth_files,
+                                                function(x) { read.table(x, header = FALSE, sep = "\t", row.names = 1, stringsAsFactors = FALSE) },
+                                                mc.cores = num_cores)
+  
   input_samples <- gsub(paste(in_path, "/", sep = ""), "", input_breadth_files)
   input_samples <- gsub(pattern, "", input_samples)
   names(input_breadth_by_sample) <- input_samples
   
-  input_breadth_by_sample[[1]]$sample <- input_samples[1]
-  input_breadth_by_sample[[1]]$gene <- rownames(input_breadth_by_sample[[1]])
-  rownames(input_breadth_by_sample[[1]]) <- NULL
-  input_breadth_combined <- input_breadth_by_sample[[1]]
+  input_breadth_by_sample_added <- parallel::mclapply(input_samples,
+                     function(x) {
+                                   working_df <-  input_breadth_by_sample[[x]]
+                                   working_df$sample <- x
+                                   working_df$gene <- rownames(working_df)
+                                   rownames(working_df) <- NULL
+                                   working_df <- working_df[, c("sample", "gene", "V7")]
+                                   colnames(working_df)[3] <- "breadth"
+                                   return(working_df)
+                                },
+                    mc.cores = num_cores)
   
-  for (i in 2:length(input_samples)) {
-    input_breadth_by_sample[[i]]$sample <- input_samples[i]
-    input_breadth_by_sample[[i]]$gene <- rownames(input_breadth_by_sample[[i]])
-    rownames(input_breadth_by_sample[[i]]) <- NULL
-    input_breadth_combined <- rbind(input_breadth_combined,  input_breadth_by_sample[[i]])
-  }
+  input_breadth_by_sample <- input_breadth_by_sample_added
+  names(input_breadth_by_sample) <- input_samples
+  rm(input_breadth_by_sample_added)
   
-  input_breadth_combined <- input_breadth_combined[, c("sample", "gene", "V7")]
-  colnames(input_breadth_combined)[3] <- "breadth"
+  input_breadth_combined <- data.table::rbindlist(input_breadth_by_sample)
+  
+  input_breadth_combined <- data.frame(input_breadth_combined)
   
   input_unique_genes <- unique(input_breadth_combined$gene)
   
+  input_unique_gene_metrics <- parallel::mclapply(input_unique_genes,
+                                                  function(gene) {
+                                                    
+                                                    gene_breadth_values <- as.numeric(input_breadth_combined[which(input_breadth_combined$gene == gene), "breadth"])
+                                                    
+                                                    return(
+                                                      
+                                                      data.frame(matrix(c(roary_formatted_pangenome[gene, "No..isolates"],
+                                                                          mean(gene_breadth_values),
+                                                                          median(gene_breadth_values),
+                                                                          max(gene_breadth_values),
+                                                                          min(gene_breadth_values),
+                                                                          sd(gene_breadth_values),
+                                                                          sd(gene_breadth_values) / mean(gene_breadth_values),
+                                                                          length(which(gene_breadth_values >= 0.1)),
+                                                                          length(which(gene_breadth_values >= 0.25)),
+                                                                          length(which(gene_breadth_values >= 0.5)),
+                                                                          length(which(gene_breadth_values >= 0.9)),
+                                                                          length(which(gene_breadth_values == 1)),
+                                                                          length(which(gene_breadth_values == 0))),
+                                                                         nrow = 1, ncol = 13)
+                                                    ))
+                                                  },
+                                                  mc.cores = num_cores)
+  
   # Get per-gene summary across samples.
-  input_breadth_summary <- data.frame(matrix(NA,
-                                             nrow = length(input_unique_genes),
-                                             ncol = 13))
+  input_breadth_summary <- data.table::rbindlist(input_unique_gene_metrics)
+  input_breadth_summary <- data.frame(input_breadth_summary)
   colnames(input_breadth_summary) <- c("ref_num", "mean", "median", "max", "min", "sd", "cv", "num_at_least_0.1",
                                        "num_at_least_0.25", "num_at_least_0.5", "num_at_least_0.9", "num_1", "num_0")
   rownames(input_breadth_summary) <- input_unique_genes
-  
-  for (input_gene in input_unique_genes) {
-    input_breadth_summary[input_gene, "ref_num"] <- roary_formatted_pangenome[input_gene, "No..isolates"]
-    input_gene_breadth_values <- input_breadth_combined[which(input_breadth_combined$gene == input_gene), "breadth"]
-    
-    input_breadth_summary[input_gene, "mean"] <- mean(input_gene_breadth_values)
-    input_breadth_summary[input_gene, "median"] <- median(input_gene_breadth_values)
-    input_breadth_summary[input_gene, "max"] <- max(input_gene_breadth_values)
-    input_breadth_summary[input_gene, "min"] <- min(input_gene_breadth_values)
-    input_breadth_summary[input_gene, "sd"] <- sd(input_gene_breadth_values)
-    input_breadth_summary[input_gene, "cv"] <- input_breadth_summary[input_gene, "sd"] / input_breadth_summary[input_gene, "mean"]
-    input_breadth_summary[input_gene, "num_at_least_0.1"] <- length(which(input_gene_breadth_values >= 0.1))
-    input_breadth_summary[input_gene, "num_at_least_0.25"] <- length(which(input_gene_breadth_values >= 0.25))
-    input_breadth_summary[input_gene, "num_at_least_0.5"] <- length(which(input_gene_breadth_values >= 0.5))
-    input_breadth_summary[input_gene, "num_at_least_0.9"] <- length(which(input_gene_breadth_values >= 0.9))
-    input_breadth_summary[input_gene, "num_1"] <- length(which(input_gene_breadth_values == 1))
-    input_breadth_summary[input_gene, "num_0"] <- length(which(input_gene_breadth_values == 0))
-  }
-  
-  # Create rounded version of metrics.
-  input_breadth_summary$mean_rounded <- round(input_breadth_summary$mean, digits = 1)
-  input_breadth_summary$median_rounded <- round(input_breadth_summary$median, digits = 1)
-  input_breadth_summary$max_rounded <- round(input_breadth_summary$max, digits = 1)
-  input_breadth_summary$min_rounded <- round(input_breadth_summary$min, digits = 1)
-  input_breadth_summary$sd_rounded <- round(input_breadth_summary$sd, digits = 1)
-  input_breadth_summary$cv_rounded <- round(input_breadth_summary$cv, digits = 1)
   
   return(list(breadth_by_sample = input_breadth_by_sample,
               breadth_all_samples = input_breadth_combined,
@@ -74,26 +82,24 @@ read_in_breadth_files <- function(in_path, pattern, roary_formatted_pangenome) {
   
 }
 
-combined_panaroo <- readRDS("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/species/highqual_genomes_panaroo/combined_panaroo_annot.rds")
-
+combined_panaroo <- readRDS("/data1/gdouglas/projects/bee_microbiome_zenodo/ref_genomes/combined_pangenome/combined_panaroo_annot.rds")
 combined_panaroo[which(is.na(combined_panaroo$No..isolates)), "No..isolates"] <- 1
 
-breadth_output <- read_in_breadth_files(in_path = "/data1/gdouglas/projects/honey_bee/Chinese_and_Ellegaard/comp_mapping/coverage_breadth",
+breadth_output <- read_in_breadth_files(in_path = "/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/comp_mapping/coverage_breadth",
                                         pattern = ".cov.bedGraph.gz",
-                                        roary_formatted_pangenome = combined_panaroo)
-
+                                        roary_formatted_pangenome = combined_panaroo,
+                                        num_cores = 50)
 
 breadth_output$breadth_summary$species <- NA
-all_species <- read.table("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/mapfiles/species.txt", stringsAsFactors = FALSE)$V1
+all_species <- read.table('/data1/gdouglas/projects/bee_microbiome_zenodo/ref_genomes/final_species_names.txt.gz',
+                          stringsAsFactors = FALSE)$V1
 for (sp in all_species) {
   breadth_output$breadth_summary[grep(sp, rownames(breadth_output$breadth_summary)), "species"] <- sp
 }
 
-
-panaroo_only_potential_core <- readRDS("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/species/core_genes/panaroo_only_potential_core.rds")
-checkm_panaroo_potential_core <- readRDS("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/species/core_genes/checkm_panaroo_passed_potential_core.rds")
+panaroo_only_potential_core <- readRDS("/data1/gdouglas/projects/bee_microbiome_zenodo/ref_genomes/core_genes/RDS/panaroo_only_potential_core.rds")
+checkm_panaroo_potential_core <- readRDS("/data1/gdouglas/projects/bee_microbiome_zenodo/ref_genomes/core_genes/RDS/checkm_panaroo_passed_potential_core.rds")
 core_genes <- c(panaroo_only_potential_core, checkm_panaroo_potential_core)
-
 
 breadth_output$breadth_summary$gene_type <- "Non-core"
 for (sp in all_species) {
@@ -107,23 +113,29 @@ breadth_output$breadth_summary <- breadth_output$breadth_summary[which(rowSums(i
 breadth_by_sample_clean <- list()
 
 for (sample in names(breadth_output$breadth_by_sample)) {
-  breadth_by_sample_clean[[sample]] <- breadth_output$breadth_by_sample[[sample]][, c("gene", "V7"), drop = FALSE]
-  colnames(breadth_by_sample_clean[[sample]]) <- c("gene", sample)
+  breadth_by_sample_clean[[sample]] <- breadth_output$breadth_by_sample[[sample]][, c('gene', 'breadth'), drop = FALSE]
+  colnames(breadth_by_sample_clean[[sample]]) <- c('gene', sample)
 }
 
 
 # Call genes as present based on breadth of coverage of at least 0.5.
-all_present <- join_all(breadth_by_sample_clean, by="gene", type='left')
+all_present <- plyr::join_all(breadth_by_sample_clean, by="gene", type='left')
 rownames(all_present) <- all_present$gene
 all_present <- all_present[, -which(colnames(all_present) == "gene")]
+
+# Also keep track of the matrix of raw breadths, in case you want to explore different cut-offs more later.
+all_breadth <- all_present
+
 all_present[all_present < 0.5] <- 0
 all_present[all_present >= 0.5] <- 1
 
+write.table(x = all_present,
+            file = '/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/comp_mapping/summary/gene_presence_0.5_breadth.tsv',
+            quote = FALSE, sep = '\t', row.names = TRUE, col.names = NA)
 
-
-saveRDS(object = all_present,
-        file = "/data1/gdouglas/projects/honey_bee/Chinese_and_Ellegaard/comp_mapping/summary/gene_presence_0.5_breadth.rds")
+write.table(x = all_breadth,
+            file = '/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/comp_mapping/summary/gene_breadth.tsv',
+            quote = FALSE, sep = '\t', row.names = TRUE, col.names = NA)
 
 saveRDS(object = breadth_output,
-        file = "/data1/gdouglas/projects/honey_bee/Chinese_and_Ellegaard/comp_mapping/summary/gene_breadth_breakdown.rds")
-
+        file = '/data1/gdouglas/projects/honey_bee/large_files_to_backup/mgs_datasets/comp_mapping/summary/gene_breadth_breakdown.rds')
---
+++ scripts/pangenome_database/cdhit_postprocess/get_cluster_member_breakdown.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+from collections import defaultdict
+
+
+def main():
+
+    print('\t'.join(['cluster', 'gene']))
+
+    with open('/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/cdhit_out/pangenome_c0.95_aL0.90_s0.90.clstr', 'r') as cluster_out:
+        for cluster_line in cluster_out:
+            if cluster_line[0] == '>':
+                current_cluster = cluster_line[1:].rstrip()
+                continue
+            
+            cluster_line_split = cluster_line.split()
+
+            gene = cluster_line_split[2][1:-3]
+            print(current_cluster + '\t' + gene)
+
+
+if __name__ == '__main__':
+    main()
