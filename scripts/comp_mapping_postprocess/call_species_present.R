8a5c0e5d33d951e9f7e601487d944f3544e06381
Gavin Douglas <gavinmdouglas@gmail.com>
2023-05-30 16:25:54 -0400

    working

---
+++ scripts/comp_mapping_postprocess/call_species_present.R
@@ -0,0 +1,76 @@
+rm(list = ls(all.names = TRUE))
+
+# Call species as present across specific samples based on core gene presence profiles.
+# Based on both 25% and 90% of core genes being present.
+
+library(ggplot2)
+
+all_present <- read.table("/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/comp_mapping/summary/gene_presence_0.5_breadth.tsv.gz",
+                          row.names = 1, stringsAsFactors = FALSE, sep = '\t', header = TRUE)
+
+core_genes <- readRDS("/data1/gdouglas/projects/bee_microbiome_zenodo/ref_genomes/core_genes/RDS/core_genes.singletons.above_len.rds")
+
+all_species <- read.table("/data1/gdouglas/projects/bee_microbiome_zenodo/ref_genomes/final_species_names.txt.gz",
+                          stringsAsFactors = FALSE)$V1
+
+# Breakdown of % core genes (of those above size cut-off) called as present per sample per species.
+all_samples <- read.table("/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/SRRs/all_five_datasets.txt.gz",
+                          stringsAsFactors = FALSE)$V1
+
+core_gene_breakdown <- data.frame(matrix(NA, nrow = length(all_species) * length(all_samples), ncol = 4))
+colnames(core_gene_breakdown) <- c("Species", "Sample", "Num_core", "Percent_core")
+
+core_gene_breakdown$Species <- rep(x = all_species, each = length(all_samples))
+core_gene_breakdown$Sample <- rep(x = all_samples, times = length(all_species))
+
+for (sp in all_species) {
+  
+  sp_core <- core_genes[[sp]]
+  
+  if (length(which(! sp_core %in% rownames(all_present))) > 0) { stop('Error - expected genes missing!') }
+  
+  for (samp in all_samples) {
+    
+    row_i <- which(core_gene_breakdown$Species == sp & core_gene_breakdown$Sample == samp)
+
+    core_gene_breakdown[row_i, "Num_core"] <- length(which(all_present[sp_core, samp] > 0))
+
+    core_gene_breakdown[row_i, "Percent_core"] <- (core_gene_breakdown[row_i, "Num_core"] / length(sp_core)) * 100
+
+  }
+
+}
+
+
+# Get species presence profile based on 25% and 90% core genes present.
+species_25per <- data.frame(matrix(0, nrow = length(all_samples), ncol = length(all_species)))
+colnames(species_25per) <- all_species
+rownames(species_25per) <- all_samples
+
+species_90per <- species_25per
+
+for (sp in all_species) {
+
+  sp_core <- core_genes[[sp]]
+
+  for (samp in all_samples) {
+ 
+    percent_core <- core_gene_breakdown[which(core_gene_breakdown$Species == sp & core_gene_breakdown$Sample == samp), "Percent_core"]
+
+    if (percent_core >= 25) {
+      species_25per[samp, sp] <- 1
+    }
+
+    if (percent_core >= 90) {
+      species_90per[samp, sp] <- 1
+    }
+
+  }
+
+}
+
+write.table(x = species_25per, file = "/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/comp_mapping/summary/species_presence_core_25percent.tsv",
+            row.names = TRUE, col.names = NA, quote = FALSE, sep = "\t")
+
+write.table(x = species_90per, file = "/data1/gdouglas/projects/bee_microbiome_zenodo/mgs_datasets/comp_mapping/summary/species_presence_core_90percent.tsv",
+            row.names = TRUE, col.names = NA, quote = FALSE, sep = "\t")
